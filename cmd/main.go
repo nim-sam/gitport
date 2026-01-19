37bf16c727e403186d0c085d2923984b0c4d195c
Nadeem Samaali <nadeem.samaali@gmail.com>
2026-01-19 14:06:48 -0500

    Fixed number of arguments

--- cmd/main.go
+++ cmd/main.go
@@ -14,13 +14,13 @@ func main() {
 
 	switch args[1] {
 	case "start":
-		if len(args) != 2 {
+		if len(args) != 3 {
 			println("Wrong number of arguments. Be sure to run\n\n\tgitport start <port>")
 			return
 		}
 		server.Start(args[2])
 	case "init":
-		if len(args) != 1 {
+		if len(args) != 2 {
 			println("gitport init doesn't take arguments")
 			return
 		}
